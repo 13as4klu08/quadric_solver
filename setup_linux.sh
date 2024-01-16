d08cb62bb494f599591eb0b310b5dbbc5f2eb54b
13as4klu08 <103316720+13as4klu08@users.noreply.github.com>
2024-01-16 07:40:00 +0100

    Delete setup_linux.sh

--- setup_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Packages should be installed by default or may not install on raspberry pi devices, in that case, manually install packages by adding --break-system-packages at pip install [packages]"
-echo "Waiting 6 seconds"
-sleep 6
-pip install numpy
-pip install colorama
-clear
-python main.py
